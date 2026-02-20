29026555c9465974cef6c001d3e15278d387e522
gamemanuel <themathnight@gmail.com>
2026-02-19 17:13:42 -0700

    added IMU.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Imu.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Imu.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 public class Imu {
+    public IMU imu;
+    double robotHeading;
+
+    public Imu(HardwareMap hardwareMap) {
+        imu = hardwareMap.get(com.qualcomm.robotcore.hardware.IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        com.qualcomm.robotcore.hardware.IMU.Parameters parameters = new com.qualcomm.robotcore.hardware.IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+    }
+
+    public double getRobotHeading() {
+        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+    }
 }
