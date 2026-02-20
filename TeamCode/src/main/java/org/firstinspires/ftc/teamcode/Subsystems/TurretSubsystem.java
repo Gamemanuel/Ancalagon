32fb69850f94282779ade2bdc7844ae611cf16dc
gamemanuel <themathnight@gmail.com>
2026-02-19 17:19:05 -0700

    added the TurretSubsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/TurretSubsystem.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class TurretSubsystem {
+    DcMotorEx Turret;
+
+    public TurretSubsystem(HardwareMap hMap) {
+        Turret = hMap.get(DcMotorEx.class, "turntable");
+    }
+
+    public void setPower(double power) {
+        Turret.setPower(power);
+    }
+}
