6f3a11fa241f4a5d1a0802ef0bb99be1dddf70e1
gamemanuel <themathnight@gmail.com>
2026-02-19 17:21:40 -0700

    added the Drivetrain commands package and added the SixWheelCMD

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Drivetrain/SixWheelCMD.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.Commands.Drivetrain;
+
+import static java.lang.Thread.sleep;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.Utils.Library.Motor.MotorGroup;
+
+public class SixWheelCMD {
+
+    Drivetrain drivetrain;
+    public SixWheelCMD(Drivetrain drivetrain) {
+        this.drivetrain = drivetrain;
+    }
+
+    /**
+     * A basic command that runs the drivetrain for a specified amount of time and then stops.
+     *
+     * @Creator Will (Finch)
+     * @param left left side of drivetrain
+     * @param right right side of drivetrain
+     * @param time how long it runs
+     */
+    public void driveBasic(MotorGroup leftSide, MotorGroup rightSide, float left, float right, long time) throws InterruptedException {
+        //set all motor powers
+        setMotors(leftSide, rightSide, left,right);
+        sleep(time); //wait for however long
+        //stop
+        setMotors(leftSide, rightSide,0,0);
+    }
+
+    /**
+     * A basic command that sets the motors to a specified power. Beware that it won't stop until
+     * said to.
+     *
+     * @Creator Will (Finch)
+     * @param backLeftSide backLeft motor of the drivetrain
+     * @param backRightSide backRight motor of the drivetrain
+     * @param frontLeftSide frontLeft motor of the drivetrain
+     * @param frontRightSide frontRight motor of the drivetrain
+     * @param leftPower left side of drivetrain
+     * @param rightPower right side of drivetrain
+     */
+    public void setMotors(DcMotorEx frontLeftSide, DcMotorEx backLeftSide, DcMotorEx frontRightSide, DcMotorEx backRightSide, float leftPower, float rightPower) {
+        frontRightSide.setPower((double) rightPower);
+        backRightSide.setPower((double) rightPower);
+        frontLeftSide.setPower((double) leftPower);
+        backLeftSide.setPower((double) leftPower);
+    }
+
+    /**
+     * A basic command that sets the motors to a specified power. Beware that it won't stop until
+     * said to.
+     *
+     * @Creator Will (Finch)
+     * @param leftSide motors or motor group left side of drivetrain
+     * @param rightSide motors or motor group of the right side of drivetrain
+     * @param leftPower left side of drivetrain
+     * @param rightPower right side of drivetrain
+     */
+    public void setMotors(MotorGroup leftSide, MotorGroup rightSide, float leftPower, float rightPower) {
+        leftSide.setPower((double) leftPower);
+        rightSide.setPower((double) rightPower);
+    }
+
+    // --- Arcade Drive Constructors ---
+    public void arcadeDrive(float forward, float turn) {
+        setMotors(drivetrain.leftSide, drivetrain.rightSide, forward - turn, forward + turn);
+    }
+
+    public void tankDrive(MotorGroup leftSide, MotorGroup rightSide,float left, float right) {
+        setMotors(leftSide, rightSide, left, right);
+    }
+
+}
